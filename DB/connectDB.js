611b2f7fe68d4c9a6b3e20cc47c9f1dbfb423f3c
krishnam384 <saikrishnamraju384@gmail.com>
2022-09-14 10:24:22 +0530

    Added mongoose

---
+++ DB/connectDB.js
@@ -0,0 +1,8 @@
+const mongoose = require("mongoose");
+
+mongoose
+  .connect("mongodb://localhost:27017/Switch", {
+    useNewUrlParser: true,
+  })
+  .then(() => console.log("Connection Established..!!"))
+  .catch((error) => console.log("Error:", error));
